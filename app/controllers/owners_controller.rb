68ecf52d03e034f0955ad187c841b5cceaeb2acc
Ian Vlahovic <banddorkfht7@gmail.com>
2018-10-16 19:15:47 +0000

    automatically backed up by learn

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -2,28 +2,36 @@ class OwnersController < ApplicationController
 
   get '/owners' do
     @owners = Owner.all
-    erb :'/owners/index' 
+    erb :'/owners/index'
   end
 
-  get '/owners/new' do 
+  get '/owners/new' do
+    @pets = Pet.all
     erb :'/owners/new'
   end
 
-  post '/owners' do 
-    
+  post '/owners' do
+    @owner = Owner.create(params[:owner])
+    @owner.pets << Pet.create(name: params["pet"]["name"]) if !params["pet"]["name"].empty?
+    redirect "owners/#{@owner.id}"
   end
 
-  get '/owners/:id/edit' do 
+  get '/owners/:id/edit' do
     @owner = Owner.find(params[:id])
+    binding.pry
     erb :'/owners/edit'
+    redirect "owners/#{@owner.id}"
   end
 
-  get '/owners/:id' do 
+  get '/owners/:id' do
     @owner = Owner.find(params[:id])
     erb :'/owners/show'
   end
 
-  patch '/owners/:id' do 
-   
+  patch '/owners/:id' do
+    params[:owner]["pet_ids"] = [] if !params[:owner].keys.include?("pet_ids")
+    @owner.pets << Pet.create(name: params["pet"]["name"]) if !params["pet"]["name"].empty?
+    redirect "owners/#{@owner.id}"
   end
+
 end
